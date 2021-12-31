92e57035d3d03e22547739f681d254152daa0db9
Sara Mohammadi <sara.mohammadi@emotion.com.ua>
2021-12-31 19:07:45 +0200

    update

--- EFCore/Repository/BiographyRepository.cs
+++ EFCore/Repository/BiographyRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Contracts.Repositories;
+using Dapper;
 using Entites;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,18 +15,26 @@ namespace EFCore.Repository
         #region Fields
         private readonly IConfiguration _configuration;
         #endregion
+
         #region Ctor
         public BiographyRepository(IConfiguration configuration)
         {
             _configuration = configuration;
         }
         #endregion
+
         #region Methods
 
 
-        public Task<int> AddAsync(Biography biography)
+        public async Task<int> AddAsync(Biography biography)
         {
-            throw new NotImplementedException();
+            var sql = ("Insert Into Biography(Description) VALUES (@Description)");
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, biography);
+                return result;
+            }
         }
 
         public Task<int> DeleteAsync(int id)
@@ -42,9 +52,15 @@ namespace EFCore.Repository
             throw new NotImplementedException();
         }
 
-        public Task<int> UpdateAsync(Biography biography)
+        public async Task<int> UpdateAsync(Biography biography)
         {
-            throw new NotImplementedException();
+            var sql = ("Update Biography Description=@Description");
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, biography);
+                return result;
+            }
         }
         #endregion
     }
