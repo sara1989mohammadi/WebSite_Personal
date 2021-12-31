f00487f1efa248d1aa7e43bb1bea16cda9aed956
Sara Mohammadi <sara.mohammadi@emotion.com.ua>
2021-12-31 19:20:58 +0200

    Update

--- EFCore/Repository/CVRepository.cs
+++ EFCore/Repository/CVRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Contracts.Repositories;
+using Dapper;
 using Entites;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,18 +15,26 @@ namespace EFCore.Repository
         #region Fields
         private readonly IConfiguration _configuration;
         #endregion
+
         #region Ctor
         public CVRepository(IConfiguration configuration)
         {
             _configuration = configuration;
         }
         #endregion
+
         #region Methods
 
 
-        public Task<int> AddAsync(CV cv)
+        public async Task<int> AddAsync(CV cv)
         {
-            throw new NotImplementedException();
+            var sql = ("Insert Into CV(Title,University,Grade) VALUES (@Title,@University,@Grade)");
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, cv);
+                return result;
+            }
         }
 
         public Task<int> DeleteAsync(int id)
@@ -42,9 +52,15 @@ namespace EFCore.Repository
             throw new NotImplementedException();
         }
 
-        public Task<int> UpdateAsync(CV cv)
+        public async Task<int> UpdateAsync(CV cv)
         {
-            throw new NotImplementedException();
+            var sql = ("Update CV Title=@Title,University=@University,Grade=@Grade");
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                var result = await connection.ExecuteAsync(sql, cv);
+                return result;
+            }
         }
         #endregion
     }
